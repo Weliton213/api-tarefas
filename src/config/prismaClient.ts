62a07564ab374f9b6df40949dd426bec3e4552fe
Weliton Almeida <almeidaweliton17@gmail.com>
2026-07-31 22:20:00 -0300

    feat: adiciona persistencia com Prisma e MySQL

---
+++ src/config/prismaClient.ts
@@ -0,0 +1,15 @@
+// src/config/prismaClient.ts
+import 'dotenv/config'
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+import { PrismaClient } from '../../generated/prisma/client'
+
+const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST!,
+    port: Number(process.env.DATABASE_PORT),
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    database: process.env.DATABASE_NAME!,
+})
+const prisma = new PrismaClient({ adapter })
+
+export default prisma
